6dad82112a0034e07a064e6ee3deeab9e28ce6c6
TomPolteq <tom.huismans@polteq.com>
2018-05-29 21:05:43 +0200

    Opdracht bootcamp Tom Huismans

--- exercises/exerciseOne.py
+++ exercises/exerciseOne.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python
 import sys
+import textwrap
 
 welcomeMessage = "Welkom bij de bootcamp toelatings opdracht. Werk alsjeblieft zorgvuldig en netjes. Succes!!"
+motivationText = "Met mijn huidige kennis ben ik instaat om scripts te schrijven en nu ben ik klaar voor de volgende stap. Door deel te nemen aan de bootcamp wil ik een aantal gestelde doelen behalen. Door zelfstandig een testframework op te kunnen zetten, verhoog ik naast mijn persoonlijke kwaliteiten ook die van Polteq. Met de technische kennis uit de bootcamp ben ik beter instaat om een professionele bijdrage te leveren aan (nieuw op te zetten) projecten binnen mijn huidige en toekomstige opdrachten. Door deze persoonlijke ontwikkeling ga ik voorbereid de toekomst tegemoet. Daarnaast lijkt het me ook ontzettend leuk om jonge, nieuwe medewerkers te kunnen begeleiden, inspireren en enthousiast te maken voor testautomatisering."
+errorMessageWelcome = "Nice try, but not quite right :)"
+errorMessageMotivation = "You are writing a motivation, not a book ;). Use max 150 words."
+# To avoid "magic" numbers
+maxCharactersPerLine = 130
+charactersLength = 91
+maxWordsMotivation = 150
+
 
 def printWelcomeText():
     welcome = welcomeMessage
     return welcome
 
 def printExercise():
-    return None
+    motivation = textwrap.fill(motivationText, width=maxCharactersPerLine)
+    return motivation
 
-if len(welcomeMessage) != 91:
-    sys.exit("Nice try, but not quite right :)")
+if len(welcomeMessage) != charactersLength:
+    sys.exit(errorMessageWelcome)
 
-print(printWelcomeText())
+if len(motivationText.split()) > maxWordsMotivation:
+    sys.exit(errorMessageMotivation)
+    
+print(printExercise())
