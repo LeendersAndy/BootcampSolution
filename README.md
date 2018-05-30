135600976b5c986faf326944d6bbc2585ece09a0
IAMBakker <ico.bakker@gmail.com>
2018-05-30 16:33:05 +0200

    Getting rid of git detached HEAD status, and actually commiting changes...

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # BootcampOpdracht
 Repository voor de Bootcamp opdracht.
+
+## How to:
+1 set up the motivation webservice found at:
+https://github.com/IAMBakker/bootcamp-motivation-ws
+
+2 run exerciseOne.py
--- exercises/exerciseOne.py
+++ exercises/exerciseOne.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python
 import sys
+import requests
 
-welcomeMessage = "Welkom bij de bootcamp toelatings opdracht. Werk alsjeblieft zorgvuldig en netjes. Succes!!"
+wsEndpoint = 'http://localhost:5000'
+welcomeMessage = requests.get(wsEndpoint + '/welcome').json()['message']
+motivation = requests.get(wsEndpoint + '/motivation').json()['message']
 
 def printWelcomeText():
     welcome = welcomeMessage
     return welcome
 
 def printExercise():
-    return None
+    return motivation
 
 if len(welcomeMessage) != 91:
     sys.exit("Nice try, but not quite right :)")
 
 print(printWelcomeText())
+print(printExercise())
