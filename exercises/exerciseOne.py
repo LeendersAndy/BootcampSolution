2dc26bf8ddcb8b2038f3a4bb6f2cd4fe20bd3d08
AnnaVos <39756406+AnnaVos@users.noreply.github.com>
2018-06-03 17:09:08 +0200

    Final version of Anna's Summer Automation Bootcamp motivation.

--- exercises/exerciseOne.py
+++ exercises/exerciseOne.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 import sys
+import random
 
-welcomeMessage = "Welkom bij de bootcamp toelatings opdracht. Werk alsjeblieft zorgvuldig en netjes. Succes!!"
+secretNumber = random.randint(1, 10)
+counter = 0
 exerciseMessageAnna = """Toen ik van 'Testautomatisering' hoorde, dacht ik: 'DAT wil ik. Dat WIL ik. Dat wil IK!' 
 
 Tijdens de Masterclass mocht ik hiervan proeven en echt: dat smaakte naar meer! Ik mocht daarna testen uitvoeren voor 
@@ -9,19 +11,44 @@ Scouting Nederland en die kans greep ik: ik volgde een cursus 'C# voor testers' 
 trots toen ik mijn eerste eigen testscript losliet op de Scouting website en het nog bleek te werken ook. 
 
 Bij NXP greep ik alles aan om testautomatisering te doen. Ik paste het Python script aan en in de ‘vrije’ uurtjes
-praatte ik wat tegen de API (vriendelijke jongen). Helaas is dit niet genoeg om daar te komen waar ik wil zijn:
+praatte ik wat tegen de API (vriendelijke jongen). Helaas is dit niet genoeg om echt te worden wat ik wil zijn:
 een echte testautomatiserings expert. Als ik mee mag doen aan de Bootcamp beloof ik plechtig me volledig in te zetten,
 een extra tandje bij te zetten en zal ik nooit een ochtendhumeur meebrengen."""
 
-def printWelcomeText():
-    welcome = welcomeMessage
-    return welcome
 
 def printExercise():
     message = exerciseMessageAnna
     return message
 
-if len(welcomeMessage) != 91:
-    sys.exit("Nice try, but not quite right :)")
+def selectQuestionMessage(argument):
+    switcher = {
+        0: """Hallo, ik ben Spoenk, Anna's Python Robot. Leuk om kennis te maken! Wil je graag haar motivatie 
+lezen voor de Summer Automation Bootcamp? Typ dan een getal tussen de 1 en de 10 en druk op enter. 
+Als je goed gokt, mag je haar motivatie lezen :) : """,
+        1: "Helaas... Probeer het nog eens: ",
+        2: "Laatste kans...: ",
+        3: "Niet goed geraden, jammer! Maar ik ben de moeilijkste niet hoor. Druk op " + str(secretNumber) + " om door te gaan..."
+    }
+    return switcher.get(argument, "Niet goed geraden, jammer! Druk op " + str(secretNumber) + " om door te gaan...")
 
-print(printExercise())
+def inputNumber():
+    number = input(selectQuestionMessage(counter))
+    return number
+
+def checkNumber(number):
+    if number == str(secretNumber):
+        return True
+    else:
+        return False
+
+def guess():
+    global counter
+    number = inputNumber()
+    check = checkNumber(number)
+    if check:
+        print(printExercise())
+    else:
+        counter += 1
+        guess();
+
+guess()
