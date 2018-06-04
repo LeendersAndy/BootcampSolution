02e168356bca74a749dc365b1bebaf854f71a7ea
Bjorn Geerding <bjornjaco.geerding@digitaslbi.com>
2018-06-04 22:01:05 +0200

    Bootcamp assignment

--- exercises/exerciseOne.py
+++ exercises/exerciseOne.py
@@ -2,15 +2,34 @@
 import sys
 
 welcomeMessage = "Welkom bij de bootcamp toelatings opdracht. Werk alsjeblieft zorgvuldig en netjes. Succes!!"
+motivationText = "Motivatie.txt"
+numberOfWords = 0
+
 
 def printWelcomeText():
     welcome = welcomeMessage
     return welcome
 
+
 def printExercise():
-    return None
+    file = open(motivationText, "r")
+    line = file.readline()
+    while line:
+        print(line.strip())
+        line = file.readline()
+    file.close()
+
+
+with open(motivationText, 'r') as file:
+    for line in file:
+        wordsList = line.split()
+        numberOfWords += len(wordsList)
 
 if len(welcomeMessage) != 91:
     sys.exit("Nice try, but not quite right :)")
 
+if numberOfWords > 150:
+    sys.exit("Nice try, but your motivation is too long! So it is boring everyone!")
+
 print(printWelcomeText())
+printExercise()
